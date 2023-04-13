01a1a23f1ab3f7fa6eb54f98a5e26d18d6744302
gminkovsky <gminkovsky@gmail.com>
2023-04-13 13:40:56 -0400

    Add files via upload

---
+++ HelloWorld.cs
@@ -0,0 +1,19 @@
+﻿using Sdl.Desktop.IntegrationApi;
+using Sdl.Desktop.IntegrationApi.Extensions;
+using Sdl.TranslationStudioAutomation.IntegrationApi;
+using Sdl.TranslationStudioAutomation.IntegrationApi.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace HelloWorldPlugin
+{
+    [Action("HelloWorldPlugin", Icon = "ExportToExcel")]
+    public class HelloWorldPlugin : AbstractAction
+    {
+        protected override void Execute()
+        {
+            MessageBox.Show("Hello, World!");
+        }
+    }
+}
